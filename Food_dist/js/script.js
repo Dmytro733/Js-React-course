63910bf50922fd69e33f6189150825303a35b3e4
Dmytro733 <ddp0496@gmail.com>
2021-01-15 15:34:17 +0200

    work with timer

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           parentTabs = document.querySelector('.tabheader__items');
@@ -35,4 +37,58 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         };
     });
+
+    //Timer
+
+    const deadLine = '2021-01-30';
+
+    function getTimeRemaining (endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)), 
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return{
+            t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function addZero (num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock (selector ,endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+              
+        function updateClock (){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = addZero(t.days),
+            hours.innerHTML = addZero(t.hours),
+            minutes.innerHTML = addZero(t.minutes),
+            seconds.innerHTML = addZero(t.seconds);
+
+            if(t.t <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer' ,deadLine);
 });
