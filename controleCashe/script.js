a83680e0faa2da2f86056a5fe085ae3021d67549
Dmytro733 <ddp0496@gmail.com>
2021-01-18 14:46:27 +0200

    modification app, add functions

--- controleCashe/script.js
+++ controleCashe/script.js
@@ -1,72 +1,109 @@
-/* 1) Переписать наш цикл for еще двумя способами и закомментировать их (еще 2 вида циклов, тренируемся)
+/* 1) Оформить расчет дневного бюджета  и вывод на экран этого значения как одну функцию (detectDayBudget)
 
-2) Проверить, чтобы все работало и не было ошибок в консоли
+2) Оформить блок кода с расчетом уровня достатка как отдельную функцию (detectLevel)
 
-3) Добавить папку со вторым уроком в свой репозиторий на GitHub */
 
-'use strict';
-    
-const money = prompt("Ваш бюджет на месяц?"),
-      time = prompt("Введите дату в формате YYYY-MM-DD");
-       
+3) Создать функцию для определения необязательных расходов (chooseOptExpenses):
 
-const appData = {
+Необходимо 3 раза спросить у пользователя “Статья необязательных расходов?”
+
+Записать ответы в объект optionalExpenses в формате Номер - Ответ.
+
+    optionalExpenses: {
+    1 : “ответ на вопрос”
+    }
+
+Вызывать функции не обязательно.
+
+4) Добавить папку со третим уроком в свой репозиторий на GitHub */
+
+"use strict";
+
+let money, time;
+
+function start(){
+    money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+    while(isNaN(money) || money == null || money == ''){
+        money = +prompt("Ваш бюджет на месяц?");
+    }
+}
+
+start();
+
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for(let i = 0; i < 2; i++){
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = +prompt("Во сколько обойдется?");
-    
-    if((typeof(a)) === 'string' && a != null && a != '' && b != null && b != ''){
-        appData.expenses[a] = b;
+function chooseExpenses(){
+    for(let i = 0; i < 2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце"),
+            b = +prompt("Во сколько обойдется?");
+        
+        if((typeof(a)) === 'string' && a != null && a != '' && b != null && b != ''){
+            appData.expenses[a] = b;
+        }else{
+            i--;
+        }
+    } 
+}
+
+chooseExpenses();
+
+function chooseOptExpenses(){
+    for(let i = 0; i < 3; i++){
+        let a = prompt("Статья необязательных расходов?");
+        
+        if((typeof(a)) === 'string' && a != null && a != '' ){
+            appData.optionalExpenses[i + 1] = a;
+        }else{
+            i--;
+        }
+    }    
+}
+
+chooseOptExpenses();
+
+function detectDayBudget(){
+    appData.monayPerDay = (appData.budget / 30).toFixed(2);
+    alert(`Ежедневний бюджет ${appData.monayPerDay}`);
+    console.log(appData);
+}
+
+detectDayBudget();
+
+function detectLevel(){
+    if(appData.monayPerDay < 100){
+        console.log('Минимальний уровень достатка');
+    }else if(appData.monayPerDay > 100 && appData.monayPerDay < 2000){
+        console.log('Средний уровень достатка');
+    }else if(appData.monayPerDay > 2000){
+        console.log('Високий уровень достатка');
     }else{
-        i--;
+        console.log('Произошла ошибка');
     }
 }
 
-/*****Цикл do while*****/
+detectLevel(); 
 
-// let i = 0;
-// do{
-//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
-//         b = +prompt("Во сколько обойдется?");
-    
-//     if((typeof(a)) === 'string' && a != null && a != '' && b != null && b != ''){
-//         appData.expenses[a] = b;
-//         i++;
-//     }else{
-//         i--;
-//     }
+function checkSavings(){
+            
+    if(appData.savings == true){
+        let save = +prompt('Какова сума накоплений'),
+            percent = +prompt('Под какой процент');
 
-// }while(i < 2);
+        appData.monthIncome = save/100/12*percent;
+        alert(`Доход в месяц с вашего депозита: ${appData.monthIncome}`);
+    }
+}
 
-/*****while*****/
-
-// while(i < 2){
-//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
-//     b = +prompt("Во сколько обойдется?");
-
-//     if((typeof(a)) === 'string' && a != null && a != '' && b != null && b != ''){
-//         appData.expenses[a] = b;
-//         i++;
-//     }else{
-//         i--;
-//     }
-// }
-
-appData.monayPerDay = appData.budget / 30;
-
-alert(`Ежедневний бюджет ${appData.monayPerDay}`);
-
-console.log(appData);
-
-   
+checkSavings();
 
 
 
